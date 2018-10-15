7a37523de38962b6a9f958d0220bff1852a5bfd0
evan9840 <44172944+evan9840@users.noreply.github.com>
2018-10-15 12:53:33 -0400

    Add files via upload

---
+++ light.ino
@@ -0,0 +1,26 @@
+void setup() {
+  pinMode(3,OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  digitalWrite(3,HIGH);
+  delay(50);
+  digitalWrite(3,LOW);
+  delay(50);
+  digitalWrite(4,HIGH);
+  delay(50);
+  digitalWrite(4,LOW);
+  delay(50);
+  digitalWrite(5,HIGH);
+  delay(50);
+  digitalWrite(5,LOW);
+  delay(50);
+  digitalWrite(6,HIGH);
+  delay(50);
+  digitalWrite(7,HIGH);
+  delay(50);
+  digitalWrite(7,LOW);
+  delay(50);
+  // put your main code here, to run repeatedly:
+}
